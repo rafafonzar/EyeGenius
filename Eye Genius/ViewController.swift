eb05e3d892dc92e090dd06ace7130f33d48bc427
rafafonzar <rafaelfonzaralb@gmail.com>
2024-05-16 16:34:32 -0300

    Initial Commit

---
+++ Eye Genius/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eye Genius
+//
+//  Created by Rafael Fonzar Albuquerque on 16/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Eye GeniusUITests/Eye_GeniusUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Eye_GeniusUITestsLaunchTests.swift
+//  Eye GeniusUITests
+//
+//  Created by Rafael Fonzar Albuquerque on 16/05/24.
+//
+
+import XCTest
+
+class Eye_GeniusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
