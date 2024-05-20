ae6a05e4390eee58dc1d4156de06861c65d5a8a5
rafafonzar <rafaelfonzaralb@gmail.com>
2024-05-20 14:57:15 -0300

    Adds navigation to grandparentButton and adds SecondScreen

---
+++ Eye Genius/SecondScreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondScreenViewController.swift
+//  Eye Genius
+//
+//  Created by Rafael Fonzar Albuquerque on 18/05/24.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+}
